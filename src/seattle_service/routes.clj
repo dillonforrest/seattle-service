f1be555c52856039c28187c0b67ee4e534739e47
Dustin Getz <dustin.getz@gmail.com>
2015-07-09 12:18:02 -0400

    Migrate to latest hypercrud-service

--- src/seattle_service/routes.clj
+++ src/seattle_service/routes.clj
@@ -1,7 +1,5 @@
 (ns seattle-service.routes
-  (:require [io.pedestal.http.body-params :as body-params]
-            [io.pedestal.http.route.definition :refer [expand-routes]]
-            [hypercrud-service.pedestal-util :as pedestal-util]
+  (:require [io.pedestal.http.route.definition :refer [expand-routes]]
             [hypercrud-service.core :as hypercrud]
             [seattle-service.page :as page]
             [seattle-service.endpoints]))
@@ -10,16 +8,5 @@
 (def routes
   (expand-routes
    `[[["/echo" {:any page/echo}]
-      ["/api" {:get [:api-get (hypercrud/mk-api-get :api-get seattle-service.endpoints/endpoints)]}
-       ^:interceptors [(body-params/body-params)
-                       pedestal-util/auto-content-type
-                       pedestal-util/combine-body-params
-                       pedestal-util/save-body-val
-                       pedestal-util/wrap-ring-response
-                       hypercrud/wrap-hypercrud
-                       ] ;; are these out of order??
-
-       ~@(map (fn [[k {:keys [query-fn typetag]}]]
-                (hypercrud/route k query-fn typetag))
-              seattle-service.endpoints/endpoints)
-       ]]]))
+      ~(hypercrud/route "/api" seattle-service.endpoints/endpoints)
+      ]]))
